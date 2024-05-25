e33eec8c820bc0b702de7080c2e09dfd294078cb
Eduardo Vicente <evicente.ti@gmail.com>
2024-05-26 00:08:01 +0200

    modificacion

--- springboot-quinoa-cafe/src/main/java/com/quinoa/cafe/springboot/app/models/dao/CategoriaDAO.java
+++
@@ -1,7 +0,0 @@
-package com.quinoa.cafe.springboot.app.models.dao;
-
-import com.quinoa.cafe.springboot.app.models.entity.Categoria;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoriaDAO extends CrudRepository<Categoria, Long> {
-}
--- springboot-quinoa-cafe/src/main/java/com/quinoa/cafe/springboot/app/models/service/IProductoService.java
+++
@@ -1,9 +0,0 @@
-package com.quinoa.cafe.springboot.app.models.service;
-
-import com.quinoa.cafe.springboot.app.models.entity.Producto;
-
-import java.util.List;
-
-public interface IProductoService {
-    public List<Producto> findByIdCategoria(Long idCategoria);
-}
