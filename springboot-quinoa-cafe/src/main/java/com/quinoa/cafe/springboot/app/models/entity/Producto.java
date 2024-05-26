e29028b2975ac4c12a541739dfe97a5bebce268f
Eduardo Vicente <evicente.ti@gmail.com>
2024-05-26 10:03:49 -0500

    agregacion de image en producto

--- springboot-quinoa-cafe/src/main/java/com/quinoa/cafe/springboot/app/models/entity/Producto.java
+++ springboot-quinoa-cafe/src/main/java/com/quinoa/cafe/springboot/app/models/entity/Producto.java
@@ -14,55 +14,52 @@ public class Producto implements Serializable {
     private String nombre;
     private String descripcion;
     private Double precio;
+    private String image;
     @Column(name = "id_categoria")
     private Long idCategoria;
     private Integer estado;
+    
+	public Long getIdProducto() {
+		return idProducto;
+	}
+	public void setIdProducto(Long idProducto) {
+		this.idProducto = idProducto;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public Double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public Long getIdCategoria() {
+		return idCategoria;
+	}
+	public void setIdCategoria(Long idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+	public Integer getEstado() {
+		return estado;
+	}
+	public void setEstado(Integer estado) {
+		this.estado = estado;
+	}
 
-    public Long getIdProducto() {
-        return idProducto;
-    }
-
-    public void setIdProducto(Long idProducto) {
-        this.idProducto = idProducto;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public Double getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(Double precio) {
-        this.precio = precio;
-    }
-
-    public Long getIdCategoria() {
-        return idCategoria;
-    }
-
-    public void setIdCategoria(Long idCategoria) {
-        this.idCategoria = idCategoria;
-    }
-
-    public Integer getEstado() {
-        return estado;
-    }
-
-    public void setEstado(Integer estado) {
-        this.estado = estado;
-    }
 }
